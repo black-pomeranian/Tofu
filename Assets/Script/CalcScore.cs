7c806c19462b8b1a54d6a7d43d39ef0643d99786
ryoneda <ryo.roma.16@gmail.com>
2022-02-20 00:34:46 +0900

    スコア計算

---
+++ Assets/Script/CalcScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalcScore : MonoBehaviour
+{
+    public int score = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        score += (int)Mathf.Ceil(Time.deltaTime * 10.0f);
+        Debug.Log(score);
+    }
+}
