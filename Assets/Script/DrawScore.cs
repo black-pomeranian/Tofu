afd653a3439f00669474df6f2b0b299247fe4aca
ryoneda <ryo.roma.16@gmail.com>
2022-02-20 01:00:47 +0900

    スコアの表示

---
+++ Assets/Script/DrawScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawScore : MonoBehaviour
+{
+    int score;
+    CalcScore cs;
+
+    void Start()
+    {
+        cs = GameObject.Find("GameMaster").GetComponent<CalcScore>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = cs.score;
+        Text score_text = this.gameObject.GetComponent<Text>();
+        score_text.text = score.ToString();
+    }
+}
