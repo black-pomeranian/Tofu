f2f844d5b0b4af08c04063471732715e8c3695ad
ryoneda <ryo.roma.16@gmail.com>
2022-02-24 23:15:58 +0900

    Start画面のUI設計中

---
+++ Assets/Script/WithKeybord.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WithKeybord : MonoBehaviour
+{
+    GameStatus gs;
+    GameObject startUI;
+
+    void Start()
+    {
+        gs = GameObject.Find("GameMaster").GetComponent<GameStatus>();
+        startUI = GameObject.Find("Start");
+    }
+    public void OnClick()
+    {
+        gs.isStart = true;
+        startUI.SetActive(false);
+    }
+}
---
+++ Assets/Script/WithToio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WithToio : MonoBehaviour
+{
+
+    GameStatus gs;
+    GameObject startUI;
+
+    void Start()
+    {
+
+        gs = GameObject.Find("GameMaster").GetComponent<GameStatus>();
+        startUI = GameObject.Find("Start");
+    }
+
+    public void OnClick()
+    {
+
+        gs.isToio = true;
+        startUI.SetActive(false);
+
+    }
+}
